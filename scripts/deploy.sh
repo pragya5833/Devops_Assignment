70d528d2d1258e77461f8d9448bc861dfe58ab44
pragya5833 <pragyabharti1312@gmail.com>
2022-10-30 02:54:06 +0530

    update deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=848417356303.dkr.ecr.ap-south-1.amazonaws.com/vprof:latest
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 848417356303.dkr.ecr.ap-south-1.amazonaws.com
 docker pull $DOCKER_IMAGE
-docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
+docker ps -q --filter "expose=8080" | xargs -r docker stop
 docker run --platform linux/amd64 -d -p 8080:8080 $DOCKER_IMAGE
