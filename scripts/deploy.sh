c612f780967d586d03ed16b07fb55427e8bf81b3
pragya5833 <pragyabharti1312@gmail.com>
2022-10-29 14:51:17 +0530

    update pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 echo "Starting to deploy docker image.."
 DOCKER_IMAGE=848417356303.dkr.ecr.ap-south-1.amazonaws.com/vprof:latest
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 848417356303.dkr.ecr.ap-south-1.amazonaws.com
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -d -p 8080:8080 $DOCKER_IMAGE
--- scripts/ec2_docker.sh
+++ scripts/ec2_docker.sh
@@ -1,6 +1,12 @@
-#! /bin/sh
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo chkconfig docker on
+#!/bin/bash
+# Install docker
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable" -y
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
+sudo usermod -aG docker ubuntu
